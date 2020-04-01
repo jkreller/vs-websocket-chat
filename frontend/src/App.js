48423a2866e3880af18208a0782591f1d2c95d81
Julian Kreller <jkreller@eos-uptrade.de>
2020-04-01 12:25:41 +0200

    Disable input zoom on mobile devices

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,16 @@ import './App.css'
 import 'react-chat-elements/dist/main.css';
 import Chat from './components/Chat/Chat'
 import Authenticator from './components/Authentication/Authenticator'
+import { Helmet } from "react-helmet";
 
 class App extends Component {
     render() {
         return (
             <div className="App full-height">
+                <Helmet>
+                    <title>VS-Websocket-Chat</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+                </Helmet>
                 <Authenticator>
                     <Chat/>
                 </Authenticator>
